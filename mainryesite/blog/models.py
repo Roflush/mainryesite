2131b7206106bcbb4903c183ec61ce1c230919b6
RoFlush <roflush@pm.me>
2022-12-17 15:27:28 -0500

    Added comments and a latest post tag

--- mainryesite/blog/models.py
+++ mainryesite/blog/models.py
@@ -45,8 +45,6 @@ class Post(models.Model):
         ]
 
 
-    
-
     def __str__(self):
         return self.title
 
--- mainryesite/blog/urls.py
+++ mainryesite/blog/urls.py
@@ -4,17 +4,21 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-      #def post_list
+    #def post_list
     path('', views.post_list, name='post_list'),
     #Class PostListView    
     #path('', views.PostListView.as_view(), name='post_list'),
+    # Path for the post.
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
             views.post_detail,
             name='post_detail'),
+    # Path for the blog share feature.
     path('<int:post_id>/share/',
           views.post_share, name='post_share'),
+    # Path for comments.
     path('<int:post_id>/comment/',
           views.post_comment, name='post_comment'),
+    # Path for list by tag
     path('tag/<slug:tag_slug>/',
          views.post_list, name='post_list_by_tag'),
 ]
