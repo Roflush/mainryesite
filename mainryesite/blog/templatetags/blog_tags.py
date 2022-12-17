01f0810c229a3033c1b226e8df1c883790eb693c
RoFlush <roflush@pm.me>
2022-12-17 17:24:25 -0500

    Added most commented on posts to sidebar

--- mainryesite/blog/templatetags/blog_tags.py
+++ mainryesite/blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from ..models import Post
+from django.db.models import Count
 
 # Required by django template library.
 # see more here: https://docs.djangoproject.com/en/4.1/howto/custom-template-tags/
@@ -16,3 +17,10 @@ def show_latest_posts(count=5):
         Can be used anywhere by calling the latest_posts html file."""
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+@register.simple_tag
+def most_commented_posts(count=5):
+    """ Gets the 5 most commented on posts. """
+    return Post.published.annotate(
+                total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
