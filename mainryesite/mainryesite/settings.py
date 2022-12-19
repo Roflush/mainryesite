ebed24fa3aaf829d0d5f986d57926634787e51cc
RoFlush <roflush@pm.me>
2022-12-19 14:50:37 -0500

    Added the start of Postgresql

--- mainryesite/mainryesite/settings.py
+++ mainryesite/mainryesite/settings.py
@@ -79,10 +79,12 @@ WSGI_APPLICATION = 'mainryesite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': 'hello1234',
         'test': {
-            'name': 'testdb',
+            'name': 'post',
         },
     }
 }
